6024e34a0bac43b1df52650108cc3c7ee8aa5aeb
Neberuslml <gallegos.h.jose@outlook.com>
2026-02-02 15:19:48 -0600

    Add '/menu' endpoint to retrieve menu items
    Added a new endpoint to serve menu items including food, drinks, and desserts.

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+app.get('/menu', (req, res) => {
+  res.json({
+    comida: [
+      { id: 1, nombre: 'Taco al Pastor', precio: 20 },
+      { id: 2, nombre: 'Taco de Asada', precio: 22 },
+      { id: 3, nombre: 'Quesadilla', precio: 25 }
+    ],
+    bebidas: [
+      { id: 4, nombre: 'Refresco', precio: 18 },
+      { id: 5, nombre: 'Agua', precio: 15 }
+    ],
+    postres: [
+      { id: 6, nombre: 'Flan', precio: 25 },
+      { id: 7, nombre: 'Gelatina', precio: 20 }
+    ]
+  });
+});
+
+
 app.listen(PORT, () => {
   console.log(`API escuchando en puerto ${PORT}`);
+
 });
